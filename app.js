1e480a37fbd0e638943ec0481d28ea62f3bad87a
Gaurav Tiwari <goodgauravtiwari@outlook.com>
2017-12-25 07:56:15 +0530

    CRUD with postgreSQL.

--- app.js
+++ app.js
@@ -35,12 +35,85 @@ app.get('/', function(req, res){
 			if(err){
 				return console.error('error running query', err);
 			}
-			res.render('index', {recipes: result.rows});
+// 			res.render('index', {recipes: result.rows});
 			done();
+			res.send({status: 200, recipes : result.rows})
 		});
 	});
 	
 
+})
+
+app.post('/add', function(req, res){
+	
+	pg.connect(connect, function(err, client, done){
+		if(err){
+			return console.error('error fetching client from pool.', err);	
+		}
+		client.query('INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)', 
+				[req.body.name, req.body.ingredients, req.body.directions], function(err, res){
+				
+				if(err){
+					return console.error('error running query', err)
+				}
+
+
+				done();
+
+				
+		});
+		
+
+	});
+
+	res.send({status: 200 , message: 'add process was successul'})
+
+})
+
+app.delete('/delete/:id' , function(req, res){
+	pg.connect(connect, function(err, client, done){
+		if(err){
+			return console.error('error fetching client from pool.', err);	
+		}
+		client.query('DELETE FROM recipes WHERE id = $1',[req.params.id], function(err, res){
+				
+				if(err){
+					return console.error('error running query', err)
+				}
+
+				done();
+					
+
+		});
+
+	});
+
+	res.send({status: 200 , message: 'delete process was successul'})
+
+		
+})
+
+app.post('/edit', function(req, res){
+		pg.connect(connect, function(err, client, done){
+		if(err){
+			return console.error('error fetching client from pool.', err);	
+		}
+		client.query('UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id = $4',[req.body.name, req.body.ingredients, req.body.directions, req.body.id], function(err, res){
+				
+				if(err){
+					return console.error('error running query', err)
+				}
+
+				done();
+					
+
+		});
+
+	});
+
+	res.send({status: 200 , message: 'edit process was successul'})
+
+	
 })
 
 //server 
